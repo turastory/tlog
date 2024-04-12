ed53042174b2f8dbf1733d4ce87bc3d138c95820
turastory <turastory@users.noreply.github.com>
2024-04-12 07:40:14 +0000

    deploy: 85e4fbddcbf0306c3e71630eed9f030c5b8ce5b2

---
+++ component---src-pages-404-tsx-3c42e644e10b0ac7f0b2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalan_playground=self.webpackChunkalan_playground||[]).push([[453],{7419:function(t,e,a){a.r(e);var n=a(9474),l=a(4511),u=a(9727);e.default=t=>{let{data:e,path:a}=t;const s=e.site.siteMetadata.title;return n.createElement(l.A,{title:s,path:a},n.createElement(u.A,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3c42e644e10b0ac7f0b2.js.map
--- component---src-pages-404-tsx-4872ac10818f071f7a18.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkalan_playground=self.webpackChunkalan_playground||[]).push([[453],{391:function(t,e,a){a.r(e);var n=a(9474),l=a(7843),u=a(6232);e.default=t=>{let{data:e,path:a}=t;const s=e.site.siteMetadata.title;return n.createElement(l.A,{title:s,path:a},n.createElement(u.A,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-4872ac10818f071f7a18.js.map
